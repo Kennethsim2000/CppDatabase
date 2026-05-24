230efa43b785e4f42a1842c9ee4ce44d9b6f6df5
kenneth <kenneth12345645@gmail.com>
2026-05-24 21:41:47 +0800

    implement insert and get tuple

--- src/table_page.cpp
+++ src/table_page.cpp
@@ -30,3 +30,73 @@ bool TablePage::has_space(size_t tuple_size)
     size_t space_left = h->free_space_ptr - slot_end;
     return space_left >= tuple_size + sizeof(Slot);
 }
+
+bool TablePage::insert_tuple(const Tuple &tuple, RID &rid)
+{
+    /*
+        STEP 1 -- check space
+        STEP 2 -- Get header
+        STEP 3 -- Move free space pointer backward
+        STEP 4 -- copy tuple bytes
+        STEP 5 -- Write slot metadata
+        STEP 6 -- create RID
+        step 7 -- increment tuple count(in header)
+        step 8 -- Mark page as dirty
+    */
+    if (!has_space(tuple.size()))
+    {
+        return false;
+    }
+
+    auto *h = header();
+
+    h->free_space_ptr -= tuple.size();
+
+    uint16_t offset = h->free_space_ptr;
+
+    std::memcpy(page_->data() + offset, tuple.data(), tuple.size());
+
+    Slot *slot_array = slots();
+
+    uint16_t slot_num = h->tuple_count;
+
+    slot_array[slot_num].offset = offset;
+    slot_array[slot_num].size = tuple.size();
+
+    rid = RID(page_->getId(), slot_num);
+
+    h->tuple_count++;
+
+    page_->mark_dirty();
+
+    return true;
+}
+
+bool TablePage::get_tuple(const RID &rid, Tuple &tuple)
+{
+    /*
+        STEP 1 -- Validate slot num
+        STEP 2 -- locate slot
+        STEP 3 -- Read tuple bytes
+        STEP 4 -- Rebuild tuple
+    */
+    auto *h = header();
+
+    if (rid.slot_num > h->tuple_count)
+    {
+        return false;
+    }
+
+    Slot *slot_array = slots();
+
+    Slot &slot = slot_array[rid.slot_num];
+    uint16_t offset = slot.offset;
+    uint16_t size = slot.size;
+
+    const uint8_t *data = page_->data() + offset;
+
+    std::vector<char> bytes(reinterpret_cast<const char *>(data), reinterpret_cast<const char *>(data + size));
+
+    tuple = Tuple(bytes);
+    return true;
+}
