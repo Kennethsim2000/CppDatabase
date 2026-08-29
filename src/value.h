dcef6eea0624ad9e936d1ae19715202fbf3e1cf3
kenneth <kenneth12345645@gmail.com>
2026-08-29 21:14:43 +0800

    add in way to represent database value

---
+++ src/value.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "types.h"
+
+namespace db
+{
+    class Value
+    {
+    public:
+        static Value Integer(int32_t value)
+        {
+            Value v;
+            v.type_ = TypeId::INTEGER;
+            v.int_value_ = value;
+            return v;
+        }
+
+        static Value Varchar(const std::string &value)
+        {
+            Value v;
+            v.type_ = TypeId::VARCHAR;
+            v.string_value_ = value;
+            return v;
+        }
+
+        TypeId type() const
+        {
+            return type_;
+        }
+
+        int32_t as_int() const
+        {
+            return int_value_;
+        }
+
+        const std::string &as_string() const
+        {
+            return string_value_;
+        }
+
+    private:
+        TypeId type_;
+
+        int32_t int_value_;
+        std::string string_value_;
+    };
+
+}
