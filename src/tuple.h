2f02a69d2bdfddbbcce27431d49ae459b130a332
kenneth <kenneth12345645@gmail.com>
2026-08-30 15:51:28 +0800

    Modify tuple.h to be able to insert structured data

--- src/tuple.h
+++ src/tuple.h
@@ -1,6 +1,10 @@
 #pragma once
+
 #include <vector>
-#include <cstring>
+#include <cstddef>
+
+#include "value.h"
+#include "schema.h"
 
 // Represents one row
 
@@ -10,7 +14,18 @@ namespace db
     {
     public:
         Tuple() = default;
-        explicit Tuple(const std::vector<char> &data) : data_(std::move(data)) {}
+
+        // Construct a tuple by serializing typed values
+        // according to the schema.
+        Tuple(
+            const std::vector<Value> &values,
+            const Schema &schema);
+
+        // Construct a tuple from already serialized bytes.
+        explicit Tuple(const std::vector<char> &data)
+            : data_(data)
+        {
+        }
 
         const char *data() const
         {
@@ -22,7 +37,16 @@ namespace db
             return data_.size();
         }
 
+        // Deserialize the value at a given column.
+        Value get_value(
+            const Schema &schema,
+            size_t index) const;
+
     private:
         std::vector<char> data_;
+
+        void append_bytes(
+            const void *data,
+            size_t size);
     };
 }
